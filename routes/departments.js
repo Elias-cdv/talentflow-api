2fc2e23ca7a71146c643f9a0653d636b1b9dfa76
Elías Cabeza de Vaca <elways38@gmail.com>
2026-06-01 21:22:58 -0500

    define deparment routes

---
+++ routes/departments.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const departmentsController = require("../controllers/departments");
+
+router.get("/", departmentsController.getAll);
+router.get("/:id", departmentsController.getSingle);
+router.post("/", departmentsController.createDepartment);
+router.put("/:id", departmentsController.updateDepartment);
+router.delete("/:id", departmentsController.deleteDepartment);
+
+module.exports = router;
